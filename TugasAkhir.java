16daad64b9543b363d788326041293f0bdfc6c9a
wilsonhutapea-ub <118100521+wilsonhutapea-ub@users.noreply.github.com>
2022-12-05 17:36:13 +0700

    Add files via upload

--- TugasAkhir.java
+++ TugasAkhir.java
@@ -1,182 +1,340 @@
-
-package tugasakhir;
-
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TugasAkhir {
-    
-    public static void main(String[] args) {
-        //1
-        Scanner sc = new Scanner(System.in);
-        
-        boolean cont = true, firstLoop = true;
-        String ansStr;
-        int ans, pointer, order;
-
-        String[] goods = {
+    static String[] goods = {
             "Colorful Nvidia GTX 1650",
             "ASRock AMD RX 6600",
             "MSI Nvidia RTX 3050",
             "ROG Strix Nvidia RTX 3080",
             "Gigabyte AMD RX 7900 XTX",
-            "Zotac Nvidia RTX 4090",
-        };
+            "Zotac Nvidia RTX 4090"
+    };
+    static int[] stock = {10,14,7,4,2,1};
 
-        int[] stock = {
-            10,
-            14,
-            7,
-            4,
-            2,
-            1
-        };
+    static int[] price = {2599000, 4150000, 5150000, 14450000, 16200000,30999000};
+    static String name;
 
-        int[] price = {
-            2599000,
-            4150000,
-            5150000,
-            14450000,
-            16200000,
-            30999000,
-        };
-        
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean cont = true, error = false;
+        String ansStr;
+        int ansInt = 0, pointer = 0, orderQty = 0, totalPrice;
+        int[] orderQtyArr = new int[goods.length];
+
+        printHeader();
+
+        System.out.println("Ralph: Hi there!");
+        System.out.println("Your response: (type 1 or 2)");
+        // TODO Hi, Ralph!
+        System.out.print("1. Hi, Ralph!\n2. Hmmm...\n> ");
+        ansStr = sc.nextLine();
+        System.out.println("Ralph: Hi!");
+        System.out.println("Ralph: I'm Ralph, your personal shopping companion. I will \nhelp you buy GPUs in this store.");
+        System.out.print("Ralph: How should I call you, friend?\n> ");
+        name = sc.nextLine();
+        System.out.printf("Ralph: Nice to meet you %s.\n", name);
+        System.out.println("Ralph: So in the scale of 1 to 10, how excited are you \nto buy PC parts from our store ?");
+        System.out.println("(1 = Not excited, 10 = Very excited)");
+        // TODO if outside 1-10
+
+        System.out.printf("%s: ",name);
+
+
+        ansInt = getInt(1,10);
+
+//        do{
+//            do{
+//                error = false;
+//                try{
+//                    ansStr = sc.nextLine();
+//                    ansInt = Integer.parseInt(ansStr);
+//                } catch (NumberFormatException e){
+//                    System.out.printf("\nRalph: It seems your input is invalid. Input must be of Integer type.\n%s: ",name);
+//                    error = true;
+//                }
+//            } while (error == true);
+//
+//            if(ansInt < 1 || ansInt > 10){
+//                System.out.printf("Ralph: It seems your input is invalid. Input must be 1-10.\n%s: ",name);
+//            }
+//        } while (ansInt < 1 || ansInt > 10);
+
+
+
+//        System.out.println(ansInt);
+
+        System.out.println();
+        if(ansInt >= 1 && ansInt <= 3){
+            System.out.println("Ralph: :(");
+        } else if(ansInt >= 4 && ansInt <= 6) {
+            System.out.println("Ralph: C'mon, cheer up a bit.");
+        } else if(ansInt >= 7 && ansInt <= 10){
+            System.out.println("Ralph: I like your spirit.");
+        } else {
+            System.out.println(":(");
+            System.out.println("alr bye");
+            System.exit(0);
+        }
+
+        System.out.println("Ralph: Here is our price list. Feel free to take a look.");
+
         do{
-            
-            if(firstLoop){
-                header();
-                System.out.println("\nI like your spirit!");
-                System.out.println("Here is our price list. Feel free to take a look.");
-            }
-            
-            
-            printMenu(goods, stock, price);
-            // batas atas
-            
-            System.out.println("So.. are you interested to order one ? (yes/no)");
-//            String ans = sc.nextLine();
-            ansStr = "yes";
-            if(!ansStr.equalsIgnoreCase("yes")){
-                break;
-            }
-            //batas bawah
+            printMenu();
 
             do{
-            System.out.println("Which GPU would you like to order ? (type in the number)");
-            ans = sc.nextInt();
-            pointer = ans-1;
-            if(ans>goods.length || ans<1)
-                System.out.println("Sorry, invalid input.");
-            } while(ans>goods.length || ans<1);
+                // TODO if input outside 1-6, don't continue, repeat.
+                System.out.printf("\nRalph: Which GPU would you like to order ? (type in the \nnumber)\n%s: ",name);
 
-            System.out.println("\nAh! so you wanna order the " + goods[pointer] + ".");
-            System.out.println("Good choice!\n");
+                ansInt = getInt(1,6);
+//                do{
+//                    try{
+//                        ansStr = sc.nextLine();
+//                        ansInt = Integer.parseInt(ansStr);
+//                    } catch (NumberFormatException e){
+//                        System.out.println("\nRalph: It seems your input is invalid. Input must be of Integer type.");
+//                        ansInt = 1;
+//                    }
+//
+//                    if(ansInt < 1 || ansInt > 6){
+//                        System.out.printf("Ralph: It seems your input is invalid. Input must be 1-6.\n%s: ",name);
+//                    }
+//                } while (ansInt < 1 || ansInt > 6);
+
+                pointer = ansInt - 1;
+                System.out.printf("\nRalph: Ah! So you want to order the %s.\n",goods[pointer]);
+                System.out.println("Ralph: Good choice!");
+
+                if(stock[pointer] == 0){
+//                    System.out.printf("\nRalph: Ah! So you want to order the %s.\n",goods[pointer]);
+//                    System.out.println("Ralph: Good choice!");
+                    System.out.printf("Ralph: But unfortunately %s is \ncurrently out of stock.\n",goods[pointer]);
+                }
+            } while (stock[pointer] == 0);
+
+
+//            System.out.println("\nRalph: Ah! so you want to order the " + goods[pointer] + ".");
+//            System.out.println("Ralph: Good choice!\n");
 
             if(stock[pointer] == 1){
-                System.out.println("You're in luck.");
-                System.out.println("Only 1 is currently in stock!");
-            } else if(stock[pointer] == 0)
-                System.out.println("But unfortunately " + goods[pointer] + " is currently out of stock.");
-            else
+                System.out.println("Ralph: You're in luck.");
+                System.out.println("Ralph: Only 1 is currently in stock!");
+            } else
                 System.out.println(stock[pointer] + " are currently in stock!");
 
-//            System.out.println("");
 
-            order = 0;
+            orderQty = 0;
 
             do{
-                System.out.print("\nHow many would you like to order?\n> ");
-                order = sc.nextInt();
-                if(order > stock[pointer]){
-                    System.out.print("Sorry. Only " + stock[pointer]);
+                System.out.print("\nRalph: How many would you like to order?\n> ");
+                do {
+                    try{
+                        ansStr = sc.nextLine();
+                        orderQty = Integer.parseInt(ansStr);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Ralph: It seems your input is invalid. Input must be of Integer type");
+                        orderQty = 9999;
+                    }
+                } while (orderQty > stock[pointer]);
+
+                if(orderQty > stock[pointer]){
+                    System.out.print("Ralph: Sorry. Only " + stock[pointer]);
                     if(stock[pointer] == 1){
                         System.out.print(" is ");
                     } else
-                    System.out.print(" are ");
+                        System.out.print(" are ");
                     System.out.println("currently in stock!");
-                } else if(order < 1)
-                    System.out.println("Order quantity must be positive.");
-            } while(order > stock[pointer] || order < 1);
-            orderArr[][]
-            System.out.printf("order quantity : %d\n\n", order);
-            stock[pointer] -= order;
-            invoice(goods, stock, price, pointer, order);
+                } else if(orderQty < 1)
+                    System.out.printf("Ralph: %s, order quantity must be positive.\n",name);
+            } while(orderQty > stock[pointer] || orderQty < 1);
 
-            System.out.println("Do you want to order again? (yes/no)");
-            sc.nextLine();
-            ansStr = sc.nextLine();
-            if(!ansStr.equalsIgnoreCase("yes")){
-                System.out.println("Thank you, come by another time.");
-                System.exit(0);
+
+            // stock
+            orderQtyArr[pointer] += orderQty;
+
+            stock[pointer] -= orderQty;
+            totalPrice = printCart(orderQtyArr);
+
+            // TODO if ansStr does not equal y/n, repeat input
+            System.out.println("Do you want to order again? (y/n)");
+            //TODO if all stock == 0, print special output & output all stock r in your card already :D
+            do{
+                ansStr = sc.nextLine();
+                if(!ansStr.contains("y") && !ansStr.contains("n")){
+                    System.out.printf("Ralph: It seems your input is invalid. Input must contain y or n.\n%s: ",name);
+                }
+
+            } while (!ansStr.contains("y") && !ansStr.contains("n"));
+
+
+            if(!ansStr.contains("y")){
+                cont = false;
+                System.out.println("Ralph: Alright. Now let's talk about payment.");
+                System.out.println("Ralph: There is 10% tax.");
+                System.out.println("Ralph: There is 5% discount if you pay with credit card.");
+                System.out.printf("Ralph: %s, what is your preferred method of payment?\n", name);
+                System.out.println("1. Cash\n2. Card");
+                System.out.printf("%s: ",name);
+
+                do{
+                    ansInt = sc.nextInt();
+                    if(ansInt < 1 || ansInt > 2){
+                        System.out.print("Ralph: Invalid input. Please try again.\n> ");
+                    }
+                } while(ansInt < 1 || ansInt > 2);
+                boolean disc = false;
+                if(ansInt == 2){
+                    int totalPrice2 = (int) (totalPrice*0.95);
+                    disc = true;
+                }
+                printInvoice(orderQtyArr, disc);
             }
-            firstLoop = false;
 
         } while(cont);
-        
+
         sc.close();
     }
-    
-    static void invoice(String[] goods, int[] stock, int[] price, int pointer, int order){
-        System.out.println("Here is your invoice.");
-        System.out.println("Please recheck your order.\n");
+
+    static void printInvoice(int[] orderQtyArr, boolean disc){
+        int typeCount = 0; // berapa jenis barang yang dipesan
+        int totalPrice = 0;
+        int grandTotal;
+        int disc_Int = 0;
+
+        System.out.println("Ralph: Here is your invoice.");
         System.out.println("======================== INVOICE ==========================");
         System.out.printf("|%-3s| %-14s | %5s | %12s |\n",
                 "No.",
                 "       Product Name       ",
                 "Qty",
                 " Total Price  ");
-        divider();
-        
-        System.out.printf("| %d | %-26s | %5d | Rp %,11d |\n",
-                    1, 
-                    goods[pointer],
-                    order,
-                    price[pointer]*order
+        printDivider();
+
+        for (int i = 0; i < orderQtyArr.length; i++) {
+            if(orderQtyArr[i] == 0)
+                continue;
+            System.out.printf("| %d | %-26s | %5d | Rp %,11d |\n",
+                    typeCount+1,
+                    goods[i],
+                    orderQtyArr[i],
+                    price[i]*orderQtyArr[i]
             );
-        divider();
+            totalPrice += price[i]*orderQtyArr[i];
+            typeCount++;
+        }
+
+        if(disc) {
+            grandTotal = (int) (totalPrice * 0.95);
+            disc_Int = (int) (0.05 * totalPrice);
+        }
+        else {
+            grandTotal = (int) (totalPrice);
+        }
+        int totalPriceAfterDisc = totalPrice-disc_Int;
+        int tax = (int)(totalPriceAfterDisc*0.1);
+        grandTotal = totalPriceAfterDisc+tax;
+        // TODO CASH if cash, no CASH if no cash.
+        // TODO discount & tax arrangement?
+        printDivider();
+        System.out.printf("| %38s | Rp %,11d |\n","TOTAL",totalPrice);
+        System.out.printf("| %38s | Rp %(,11d |\n","DISCOUNT",disc_Int);
+        System.out.printf("| %38s | Rp %,11d |\n","TAX",tax);
+        printDivider();
+        System.out.printf("| %38s | Rp %,11d |\n","GRAND TOTAL",grandTotal);
+        printDivider();
     }
-    
-    static void printMenu(String[] goods, int[] stock, int[] price){
-        
+    static int printCart(int[] orderQtyArr){
+        int typeCount = 0;
+        int totalPrice = 0;
+        System.out.println("\nRalph: Here is your cart. Please recheck your order.\n");
+        System.out.println("========================== CART ===========================");
+        System.out.printf("|%-3s| %-14s | %5s | %12s |\n",
+                "No.",
+                "       Product Name       ",
+                "Qty",
+                " Total Price  ");
+        printDivider();
+
+        for (int i = 0; i < orderQtyArr.length; i++) {
+            if(orderQtyArr[i] == 0)
+                continue;
+            System.out.printf("| %d | %-26s | %5d | Rp %,11d |\n",
+                    typeCount+1,
+                    goods[i],
+                    orderQtyArr[i],
+                    price[i]*orderQtyArr[i]
+            );
+            totalPrice += price[i]*orderQtyArr[i];
+            typeCount++;
+        }
+
+        printDivider();
+        System.out.printf("| %38s | Rp %,11d |\n","TOTAL",totalPrice);
+        printDivider();
+        return totalPrice;
+    }
+
+    static void printMenu(){
+
         System.out.println("\n======================= LIST HARGA ========================");
         System.out.printf("|%-3s| %-14s | %5s | %12s |\n",
                 "No.",
                 "       Product Name       ",
                 "Stock",
                 "     Price    ");
-        divider();
-        
+        printDivider();
+
         for (int i = 0; i < goods.length; i++) {
             System.out.printf("| %d | %-26s | %5d | Rp %,11d |\n",
-                    i+1, 
+                    i+1,
                     goods[i],
                     stock[i],
                     price[i]
-            );  
+            );
         }
-        divider();
-        
+        printDivider();
+
     }
-    
-    static void divider(){
+
+    static void printDivider(){
         System.out.println("===========================================================");
     }
-    
-    static void header(){
+
+    static void printHeader(){
         System.out.println("======================= WELCOME TO ========================");
-        System.out.println("================= RAYWILFRED GAMING SHOP ==================");
-        System.out.println("");
-        System.out.println("Here you can buy PC parts like CPU, SSD, Mobo, and many\nmore.");
-        System.out.println("");
-        System.out.println("Hi there! My name is Raywilfred. I will help you buy a GPU\nin this store.");
-        System.out.println("So in the scale of 1 to 10, how interested are you to take\nhome your dream GPU ?");
-        
-        Scanner sc = new Scanner(System.in);
-//        String ans = sc.nextLine();
-        String ans = "10";
-        if(!ans.equalsIgnoreCase("10")){
-            System.out.println("Thank you, come by another time.");
-            System.exit(0);
-        }
+//        System.out.println("================= RAYWILFRED GAMING SHOP ==================");
+        System.out.println("================ RAYWILFRED COMPUTER SHOP =================");
+        System.out.println("\nHere you can buy PC parts like CPU, SSD, Mobo, and build \nyour dream PC.\n");
     }
-    
+
+    static int getInt(int batasBawah, int batasAtas){
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        boolean error;
+
+        do{
+            do{
+                error = false;
+                try{
+                    String ansStr = sc.nextLine();
+                    result = Integer.parseInt(ansStr);
+                } catch (NumberFormatException e){
+                    System.out.printf("\nRalph: It seems your input is invalid. Input must be of \nInteger type.\n%s: ",name);
+                    error = true;
+                }
+            } while (error);
+
+            if(result < batasBawah || result > batasAtas){
+                System.out.printf("Ralph: It seems your input is invalid. Input must be %d-%d.\n",batasBawah,batasAtas);
+                System.out.printf("%s: ", name);
+            }
+        } while (result < batasBawah || result > batasAtas);
+
+        return result;
+    }
 }
