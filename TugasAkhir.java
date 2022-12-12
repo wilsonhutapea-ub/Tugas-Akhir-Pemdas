2e2abc26a7e5147422f8becda7f5010442acafc5
wilsonhutapea-ub <118100521+wilsonhutapea-ub@users.noreply.github.com>
2022-12-12 11:04:07 +0700

    Add files via upload

--- TugasAkhir.java
+++ TugasAkhir.java
@@ -27,10 +27,10 @@ public class TugasAkhir {
 
         System.out.println("Ralph: Hi there!");
         System.out.println("Ralph: I'm Ralph, your personal shopping companion. I will \nhelp you buy GPUs in this store.");
-        System.out.print("Ralph: How should I call you, friend?\n> ");
+        System.out.print("\nRalph: How should I call you, friend?\n> ");
         name = sc.nextLine();
-        System.out.printf("Ralph: Nice to meet you %s.\n", name);
-        System.out.println("Ralph: So in the scale of 1 to 10, how excited are you \nto buy PC parts from our store ?");
+        System.out.printf("\nRalph: Nice to meet you %s.\n", name);
+        System.out.println("\nRalph: So in the scale of 1 to 10, how excited are you \nto buy PC parts from our store ?");
         System.out.println("(1 = Not excited, 10 = Very excited)");
         System.out.printf("%s: ",name);
 
@@ -42,7 +42,7 @@ public class TugasAkhir {
         else if(ansInt >= 7 && ansInt <= 10)
             System.out.println("Ralph: I like your spirit.");
 
-        System.out.println("\nRalph: Here is our price list. Feel free to take a look.\n");
+        System.out.println("\nRalph: Here is our price list. Feel free to take a look.");
 
         do{
             printMenu();
@@ -116,7 +116,7 @@ public class TugasAkhir {
             if(soldOut || !ansStr.contains("y") && !ansStr.contains("Y")){
                 cont = false;
                 System.out.println("\nRalph: Alright. Now let's talk about payment.");
-                System.out.println("Ralph: There is 10% tax, and there is 5% discount if you \npay with credit card.");
+                System.out.println("\nRalph: There is 10% tax, and there is 5% discount if you \npay with credit card.");
                 System.out.printf("\nRalph: What is your preferred method of payment, %s?\n", name);
                 System.out.println("1. Cash\n2. Card");
                 System.out.printf("%s: ",name);
@@ -131,7 +131,7 @@ public class TugasAkhir {
         } while(cont);
 
         System.out.println("\nRalph: Thank you for shopping in our store. Feel free to \ncome back next time, where I will guide you again to find \nthe best quality PC Parts.");
-        System.out.printf("Ralph: Goodbye and have a nice day, %s.\n", name);
+        System.out.printf("\nRalph: Goodbye and have a nice day, %s.\n", name);
 
         sc.close();
     }
@@ -150,7 +150,7 @@ public class TugasAkhir {
         System.out.printf("|%-3s| %-14s | %5s | %12s |\n",
                 "No.",
                 "       Product Name       ",
-                "Qty",
+                "Qty ",
                 " Total Price  ");
 
         printDivider();
@@ -199,7 +199,7 @@ public class TugasAkhir {
         System.out.printf("|%-3s| %-14s | %5s | %12s |\n",
                 "No.",
                 "       Product Name       ",
-                "Qty",
+                "Qty ",
                 " Total Price  ");
 
         printDivider();
